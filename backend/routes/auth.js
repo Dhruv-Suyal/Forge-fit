8caf67b6329d1b01652844f3092a59a36c546a65
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2026-05-24 10:53:41 +0530

    done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,7 +18,7 @@ router.get("/google/callback",
         res.cookie("token", token, {
             httpOnly:true,
             secure:process.env.NODE_ENV === "production",
-            sameSite:"lax",
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         if(user.onboardingCompleted){
