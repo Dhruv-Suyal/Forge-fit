2c39e51537546c94e576423dec8da836fe2bca82
Dhruv <dhruvsuyal0@gmail.com>
2026-05-24 11:06:06 +0530

    Update Google OAuth callback URL for production

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -6,7 +6,7 @@ const User = require("../models/user");
 passport.use(new GoogleStrategy ({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://forge-fit.onrender.com/api/auth/google/callback"
 },
 
 async (accessToken, refreshToken, profile, done) => {
